2360cb8eac89c2f5d98f8e3305a924dfb2354463
nicoaradan <jesus.saves1>
2014-10-06 15:31:38 +0200

    Added test for the Comment model. The tests added are for the comment approval functionality.

---
+++ protected/tests/unit/CommentTest.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: d.nicoara
+ * Date: 06/10/14
+ * Time: 15:05
+ */
+class CommentTest extends CDbTestCase
+{
+	public $fixtures = array(
+		'posts' => 'Post',
+		'comments' => 'Comment'
+	);
+
+	public function testApprove()
+	{
+		$comment = new Comment();
+		$comment->setAttributes(
+			array(
+				'content' => 'comment 1',
+				'status' => Comment::STATUS_PENDING,
+				'createTime' => time(),
+				'author' => 'me',
+				'email' => 'me@example.com'
+			), false);
+
+		$this->assertTrue($comment->save(false));
+
+		$comment = Comment::model()->findByPk($comment->id);
+		$this->assertTrue($comment instanceof Comment);
+		$this->assertEquals(Comment::STATUS_PENDING, $comment->status);
+
+		$comment->aprove();
+		$this->assertEquals(Comment::STATUS_PENDING, $comment->status);
+		$comment = Comment::model()->findByPk($comment->id);
+		$this->asserEquals(Comment::STATUS_APPROVED, $comment->status);
+	}
+}
