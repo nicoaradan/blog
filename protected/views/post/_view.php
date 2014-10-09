913706454a5256ca1c3591c00ae44d68874d3edf
nicoaradan <jesus.saves1>
2014-10-09 13:50:54 +0200

    Modified the model for the partial /post/_view. Now the status is displayed as a text not as a number.

--- protected/views/post/_view.php
+++ protected/views/post/_view.php
@@ -22,7 +22,7 @@
 	<br />
 
 	<b><?php echo CHtml::encode($data->getAttributeLabel('status')); ?>:</b>
-	<?php echo CHtml::encode($data->status); ?>
+    <?php echo CHtml::encode($data->mapStatus->name); ?>
 	<br />
 
 	<b><?php echo CHtml::encode($data->getAttributeLabel('create_time')); ?>:</b>
