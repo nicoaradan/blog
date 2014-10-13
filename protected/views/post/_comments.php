8558dd45d652b545071cccef1a69c80920046171
nicoaradan <jesus.saves1>
2014-10-13 13:55:31 +0200

    Code format.

--- protected/views/post/_comments.php
+++ protected/views/post/_comments.php
@@ -1,26 +1,26 @@
 <?php foreach ($comments as $comment): ?>
     <?php if (($comment->post_id == $post->id_tbl_post) &&
-        ($comment->status == 2)
+        ($comment->status == Comment::STATUS_APPROVED)
     ): ?>
-<div class="comment" id="c<?php $comment->id; ?>">
+        <div class="comment" id="c<?php $comment->id; ?>">
 
-	<?php echo CHtml::link(
-		"#{$comment->id}", $comment->getUrl($post), array(
-			'class' => 'cid',
-			'title' => 'Permalink to this comment'
-		));?>
+            <?php echo CHtml::link(
+                "#{$comment->id}", $comment->getUrl($post), array(
+                    'class' => 'cid',
+                    'title' => 'Permalink to this comment'
+                ));?>
 
-	<div class="author">
-        <?php echo htmlentities($comment->author); ?>
-	</div>
+            <div class="author">
+                <?php echo htmlentities($comment->author); ?>
+            </div>
 
-	<div class="time">
-        <?php echo htmlentities($comment->create_time); ?>
-	</div>
+            <div class="time">
+                <?php echo htmlentities($comment->create_time); ?>
+            </div>
 
-	<div class="content">
-        <?php echo htmlentities($comment->content); ?>
-	</div>
-</div>
+            <div class="content">
+                <?php echo htmlentities($comment->content); ?>
+            </div>
+        </div>
     <?php endif; ?>
 <?php endforeach; ?>
