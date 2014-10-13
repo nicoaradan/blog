b8594c8763532475130b89bad54db4e1b5eaa4e0
nicoaradan <jesus.saves1>
2014-10-13 12:10:25 +0200

    Deleted some commented code inside UserIdentity class.

--- protected/components/UserIdentity.php
+++ protected/components/UserIdentity.php
@@ -19,20 +19,6 @@ class UserIdentity extends CUserIdentity
      */
     public function authenticate()
     {
-//		$users=array(
-//			// username => password
-//			'demo'=>'demo',
-//			'admin'=>'admin',
-//		);
-//
-//		if(!isset($users[$this->username]))
-//			$this->errorCode=self::ERROR_USERNAME_INVALID;
-//		elseif($users[$this->username]!==$this->password)
-//			$this->errorCode=self::ERROR_PASSWORD_INVALID;
-//		else
-//			$this->errorCode=self::ERROR_NONE;
-//		return !$this->errorCode;
-
         $username = strtolower($this->username);
         $user     = User::model()->find('LOWER(username)=?', array($username));
         if ($user == null) {
