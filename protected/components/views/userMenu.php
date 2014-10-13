4a7a6adea4a10d82e289cfcf96e4d4f2b14d19fe
nicoaradan <jesus.saves1>
2014-10-13 15:11:05 +0200

    Manage post, manage comments and approve comments is now available only for the demo user.

--- protected/components/views/userMenu.php
+++ protected/components/views/userMenu.php
@@ -1,6 +1,9 @@
 <ul>
     <li><?php echo CHtml::link('Create new Post', array('post/create')); ?></li>
+    <!-- TODO modify this using roles from Yii framework -->
+    <?php if (Yii::app()->user->name == 'demo'): ?>
     <li><?php echo CHtml::link('Manage posts', array('post/admin')); ?></li>
     <li><?php echo CHtml::link('Manage comments', array('comment/index')); ?></li>
+    <?php endif; ?>
     <li><?php echo CHtml::link('Logout', array('site/logout')); ?></li>
 </ul>
--- protected/views/comment/view.php
+++ protected/views/comment/view.php
@@ -2,37 +2,52 @@
 /* @var $this CommentController */
 /* @var $model Comment */
 
-$this->breadcrumbs=array(
-	'Comments'=>array('index'),
-	$model->id,
+$this->breadcrumbs = array(
+    'Comments' => array('index'),
+    $model->id,
 );
 
-$this->menu=array(
-	array('label'=>'List Comment', 'url'=>array('index')),
-	array('label'=>'Create Comment', 'url'=>array('create')),
-	array('label'=>'Update Comment', 'url'=>array('update', 'id'=>$model->id)),
-	array('label'=>'Delete Comment', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->id),'confirm'=>'Are you sure you want to delete this item?')),
-	array('label'=>'Manage Comment', 'url'=>array('admin')),
+$this->menu = array(
+    array('label' => 'List Comment', 'url' => array('index')),
+    array('label' => 'Create Comment', 'url' => array('create')),
+    array('label' => 'Update Comment', 'url' => array('update', 'id' => $model->id)),
+    array(
+        'label' => 'Delete Comment',
+        'url' => '#',
+        'linkOptions' => array(
+            'submit' => array('delete', 'id' => $model->id),
+            'confirm' => 'Are you sure you want to delete this item?'
+        )
+    ),
+    array('label' => 'Manage Comment', 'url' => array('admin')),
 );
 ?>
 
-<h1>View Comment #<?php echo $model->id; ?></h1>
+    <h1>View Comment #<?php echo $model->id; ?></h1>
 
 <?php $this->widget('zii.widgets.CDetailView', array(
-	'data'=>$model,
-	'attributes'=>array(
-		'id',
-		'content',
+    'data' => $model,
+    'attributes' => array(
+        'id',
+        'content',
         array(
             'name' => 'status',
             'value' => $model->statusValue->name
         ),
-		'create_time',
-		'author',
-		'email',
-		'url',
-	),
+        'create_time',
+        'author',
+        'email',
+        'url',
+    ),
 )); ?>
-<div id="comments">
-    <?php echo CHtml::link('Approve', Yii::app()->createUrl('/comment/approve', array('id' => $model->id))); ?>
-</div>
+    <br/>
+    <!-- TODO modify this using roles from Yii framework -->
+<?php if (Yii::app()->user->name == 'demo'): ?>
+
+    <div id="comments">
+        <!-- TODO add the inline CSS to a file. -->
+        <?php echo CHtml::link('Approve', Yii::app()->createUrl('/comment/approve', array('id' => $model->id)),
+            array('style' => 'text-decoration:none;')); ?>
+    </div>
+
+<?php endif; ?>
