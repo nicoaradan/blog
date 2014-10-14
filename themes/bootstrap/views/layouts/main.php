69f56da63d4ceb1a6959858c4a41630c9cdf24b7
Dan Nicoara <dan.nicoara@westwing.de>
2014-10-14 16:06:39 +0200

    Test Button removed.

--- themes/bootstrap/views/layouts/main.php
+++ themes/bootstrap/views/layouts/main.php
@@ -20,7 +20,6 @@
             'class' => 'bootstrap.widgets.TbMenu',
             'items' => array(
                 array('label' => 'Home', 'url' => array('/')),
-                array('label' => 'Test', 'url' => array('/')),
                 array('label' => 'About', 'url' => array('/site/page', 'view' => 'about')),
                 array('label' => 'Contact', 'url' => array('/site/contact')),
                 array('label' => 'Login', 'url' => array('/site/login'), 'visible' => Yii::app()->user->isGuest),
