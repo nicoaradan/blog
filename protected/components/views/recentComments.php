b40018ec29f217279c8096fdaa8b712c202a66c7
Dan Nicoara <dan.nicoara@westwing.de>
2014-10-15 10:11:39 +0200

    Refactored the layout of the recent comments.
    Added some Bootstrap CSS classes and also some html elements.

--- protected/components/views/recentComments.php
+++ protected/components/views/recentComments.php
@@ -1,9 +1,13 @@
-<ul>
+<div class="list-group">
     <?php foreach ($this->getRecentComments() as $comment): ?>
-        <li>
-            <?php echo $comment->content; ?>
-            <?php echo $comment->getAuthorLink(); ?> on <?php echo CHtml::link(
-                $comment->post->title, $comment->getUrl()); ?>
-        </li>
+        <a href="#" class="list-group-item">
+            <h4 class="list-group-item-heading"><?php echo $comment->content; ?></h4>
+
+            <p class="list-group-item-text">
+                <?php echo $comment->getAuthorLink(); ?>
+                on <?php echo CHtml::link(
+                    $comment->post->title, $comment->getUrl()); ?>
+            </p>
+        </a>
     <?php endforeach; ?>
-</ul>
+</div>
