23bdc5d21d36d9a8ede35bd708e9c6e5aa9551df
Dan Nicoara <dan.nicoara@westwing.de>
2014-10-15 10:17:59 +0200

    Small refactoring to recentComments.

--- protected/components/views/recentComments.php
+++ protected/components/views/recentComments.php
@@ -1,13 +1,13 @@
 <div class="list-group">
     <?php foreach ($this->getRecentComments() as $comment): ?>
-        <a href="#" class="list-group-item">
+        <span>
             <h4 class="list-group-item-heading"><?php echo $comment->content; ?></h4>
 
             <p class="list-group-item-text">
-                <?php echo $comment->getAuthorLink(); ?>
+                <i><?php echo $comment->getAuthor(); ?></i>
                 on <?php echo CHtml::link(
                     $comment->post->title, $comment->getUrl()); ?>
             </p>
-        </a>
+        </span>
     <?php endforeach; ?>
 </div>
