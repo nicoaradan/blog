485ac081213d3e928cdd368152edc671bff08b20
Dan Nicoara <dan.nicoara@westwing.de>
2014-10-15 10:10:42 +0200

    Added new CSS classes and htmlOptions inside column2.php file.
    These are used to enhance the UI.

--- themes/bootstrap/views/layouts/column2.php
+++ themes/bootstrap/views/layouts/column2.php
@@ -12,12 +12,36 @@
                 <?php
                 $this->beginWidget('zii.widgets.CPortlet', array(
                     'title' => 'Operations',
+                    'titleCssClass' => 'alert alert-info',
+                    'htmlOptions' => array('role' => 'alert')
                 ));
                 $this->widget('bootstrap.widgets.TbMenu', array(
-                    'items' => $this->menu,
-                    'htmlOptions' => array('class' => 'operations'),
+                    'items' => $this->menu
                 ));
                 $this->endWidget();
+
+                $this->widget(
+                    'TagCloud', array(
+                        'maxTags' => Yii::app()->params['tagCloudCount'],
+                        'titleCssClass' => 'alert alert-info',
+                        'htmlOptions' => array('role' => 'alert')
+                    ));
+
+                $this->widget(
+                    'RecentComments', array(
+                        'maxComments' => Yii::app()->params['maxComments'],
+                        'titleCssClass' => 'alert alert-info',
+                        'htmlOptions' => array('role' => 'alert')
+                    ));
+
+                $this->widget(
+                    'ArchivedPostPortlet', array(
+                        'limit' => 10,
+                        'titleCssClass' => 'alert alert-info',
+                        'htmlOptions' => array('role' => 'alert')
+                    )
+                );
+
                 ?>
             </div>
             <!-- sidebar -->
